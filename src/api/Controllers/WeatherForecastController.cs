a7bc7bb6204b12a875b5c065c7033ddf2b57585a
Warwick Stone <warwick_stone@fastmail.com>
2021-05-08 15:39:43 +1000

    add boilerplate to namespace

--- src/api/Controllers/WeatherForecastController.cs
+++ src/api/Controllers/WeatherForecastController.cs
@@ -1,11 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace api.Controllers
+namespace boilerplate.api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
--- src/api/WeatherForecast.cs
+++ src/api/WeatherForecast.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace api
+namespace boilerplate.api
 {
     public class WeatherForecast
     {
